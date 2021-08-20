c1abcc4481362ad77eda3c3521d3b045de754f1e
megaton111 <lkh890111@gmail.com>
2021-08-20 13:38:51 +0900

    기본적인 기능, 디자인 완료

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,77 @@
 <template>
   <div id="app">
-    <Header />
-    <router-link to="/main">main</router-link>
-    <router-link to="/sub">sub</router-link>
-    <router-view></router-view>
-    <Footer />
+    
+    <header class="todo-header">
+      <div class="date"><h1>{{ date }}<br /><span>{{ day }}</span></h1></div>
+      <div class="count">할일 <span>{{ todoCount }}</span>개 남음</div>
+    </header>
+
+    <div class="todo-list">
+      <div class="todo-list-item" v-for="(item , idx) in todos" :key="idx" :class="{ complete : item.complete }">
+        <label :for="idx+'todo'">
+          <input type="checkbox" v-model="item.complete" :id="idx+'todo'" />
+          <font-awesome-icon v-if="item.complete" class="ic_check" :icon="['fa', 'check-circle' ]" />
+          <i v-else class="ic_check_circle" :icon="['far', 'check-circle' ]" />
+        </label>
+        {{ item.text }}
+        <button class="btn_delete" type="button" @click="deleteEventHandler( idx )"><font-awesome-icon :icon="['far', 'trash-alt' ]" /></button>
+      </div>
+    </div>
+
+    <div class="todo-input">
+      <div class="todo-input-bx">
+        <input type="text" v-model="inputText" />
+      </div>
+      <button type="button" @click="addEventHandler" class="btn_add">
+        <font-awesome-icon :icon="['fa', 'plus' ]" />
+      </button>
+    </div>
+
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
-    Header, Footer
+  } ,
+  data () {
+    return {
+      todos : [
+        // { text : '개발 세팅하기' , complete : false } ,
+        // { text : '미국 여행 비행기 예약하기' , complete : false } ,
+        // { text : '프린트 출력' , complete : false } ,
+      ] ,
+      inputText : '' , 
+      date : '' , 
+      day : '' , 
+    }
+  } ,
+  methods : {
+    addEventHandler() {
+      if( this.inputText == '' ) {
+        alert( '할일을 입력하세요' ) ; 
+      } else {
+        this.todos.push({ text : this.inputText, complete : false }) ; 
+        this.inputText = '' ; // 입력값 초기화
+      }
+    } ,
+    deleteEventHandler( idx ) {
+      this.todos.splice( idx, 1 ) ; 
+    } 
+  } , 
+  created(){
+    let t = new Date() ;
+    const WEEKDAY = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
+    this.date = t.getFullYear() + '년 ' + ( t.getMonth() + 1 ) + '월 ' + t.getDate() + '일' ; 
+    this.day = WEEKDAY[t.getDay()];
+
+  } , 
+  computed : {
+    todoCount () {
+      return this.todos.filter(item=>!item.complete).length ;
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'; 
 import store from './store/index'; 
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+import { faLink, faRedo, faUndo, faCheckCircle as fabCheckCircle, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faTrashAlt, faCheckCircle } from "@fortawesome/free-regular-svg-icons";
+
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faTrashAlt, faCheckCircle, fabCheckCircle, faPlus );
+library.add(faLink, faRedo, faUndo);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
